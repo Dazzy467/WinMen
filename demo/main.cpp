0e671cf28bcf6f97e7f55b70ae494bd2b8476721
David Yusuf De Waskito <80533425+Dazzy467@users.noreply.github.com>
2021-10-23 22:03:54 +0800

    Updated, this now shows how to use input form

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,14 +1,14 @@
 #include "input.h"
-#include "Menu.h"  
+#include "Menu.h"
 
 int main()
 {
     bool isRun = true;
+    int ins = 0;
+    std::string in_str;
+    std::string* ptr_str = &in_str;
     Menu men;
-    input keys[3];
-    keys[0].setKey(VK_DOWN);
-    keys[1].setKey(VK_UP);
-    keys[2].setKey(VK_RETURN);
+    input keys[3]{VK_DOWN, VK_UP, VK_RETURN};
 
     men.showConsoleCursor(false);
     men++;
@@ -16,33 +16,44 @@ int main()
     men--;
     std::cout << men.GetIndex() << std::endl;
 
-    men.Set({"Start", "Option", "Exit"});
+    men.Set({"Start", "Input test", "Exit"});
     men.SetPosition(30, 5);
-    men.selectMenuAttr(Menu::Attribute::A_HIGHLIGHT, 0xF1);                                                       
-    men.draw();                                   
-    printAt(40,13,1);
+    men.selectMenuAttr(Menu::Attribute::A_HIGHLIGHT, 0xF1);
+    men.draw();
+    printAt(40, 13, in_str);
     do
     {
         keys[0].doIfkeyPressed([&]()
                                {
                                    if (men.GetIndex() < men.GetMaxAvailIndex())
                                        men++;
-                                  
-                                   men.draw();
-                                   //men.refresh();
+
+                                   //men.draw();
+                                   men.refresh();
                                });
         keys[1].doIfkeyPressed([&]()
                                {
                                    if (men.GetIndex() > 0)
                                        men--;
-                                   
-                                   men.draw();
-                                   //men.refresh();
+
+                                   //men.draw();
+                                   men.refresh();
                                });
         keys[2].doIfkeyPressed([&]()
                                {
-                                   if (men.GetIndex() == 2)
+                                   switch (men.GetIndex())
+                                   {
+                                   case 1:
+                                       men.InputForm_str(std::cin, "Ganti jadi: ",*ptr_str);
+                                       printAt(40, 13, in_str);
+                                       break;
+                                   case 2:
                                        isRun = false;
+                                       break;
+
+                                   default:
+                                       break;
+                                   }
                                });
         Sleep(10);
     } while (isRun);
